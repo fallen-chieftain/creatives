32c9830fc4d8c30a8ed28a25771f6853c12fd6a2
Alex <81800852+fallen-chieftain@users.noreply.github.com>
2021-04-02 09:25:18 +0000

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// scroll button start
+mybutton = document.getElementById("btn_scroll");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// scroll button end
